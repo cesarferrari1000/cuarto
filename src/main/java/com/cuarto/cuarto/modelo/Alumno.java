c757560f24acbf7a51d74a26e8f5311cea50c01d
cesar <cesarferrari1000>
2026-08-21 21:12:03 -0600

    primer commit

--- src/main/java/com/cuarto/cuarto/modelo/Alumno.java
+++ src/main/java/com/cuarto/cuarto/modelo/Alumno.java
@@ -25,6 +25,9 @@ public class Alumno {
         this.apellidoM = apellidoM;
         this.matricula = matricula;
     }
+    public Alumno(){
+
+    }
 
     public Long getId() {
         return id;
